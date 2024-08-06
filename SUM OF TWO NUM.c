4d534238899d59853d12a2a4679b77d4dbf3a342
RAJ-CHOUBEY <rajchoubey578@gmail.com>
2024-08-07 02:19:10 +0530

    Add files via upload

---
+++ SUM OF TWO NUM.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1,n2,sum;
+    printf("/n the value of number = ");
+    scanf("%d",&n1);
+    printf("/n the value of number = ");
+    scanf("%d",&n2);
+    sum=n1+n2;
+    printf("the sum is %d" ,sum);
+return 0;
+}
